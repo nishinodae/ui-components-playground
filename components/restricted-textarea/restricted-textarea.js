642592524f97c785444b76576aa66ee617124810
nishinodae <masyitahsalwa@gmail.com>
2026-01-18 06:36:32 +0800

    feat: update function to change color back to original when textlength < maxChar

--- components/restricted-textarea/restricted-textarea.js
+++ components/restricted-textarea/restricted-textarea.js
@@ -5,10 +5,11 @@ const maxChar = 250;
 textAreaEl.addEventListener('input', () => {
     const textLength = textAreaEl.value.length;
     if (textLength === maxChar) {
-        textAreaEl.style.color = 'red';
-        textAreaEl.style.outlineColor = 'red';
-        textAreaEl.style.borderColor = 'red';
+        textAreaEl.classList.add('exceed-limit');
         counter.style.color = 'red';
+    } else {
+        textAreaEl.classList.remove('exceed-limit');
+        counter.style.color = 'black';
     }
     counter.textContent = `${textLength} / ${maxChar}`;
 })
